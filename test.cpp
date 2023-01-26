edf5a1b101d295ae65865278ae4acadb92817268
Linda Ngo <111800906+ling3855@users.noreply.github.com>
2023-01-26 13:01:09 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
+#include <vector> 
+
+TEST_CASE ("Factorials are computed", "[factorial]"){
+    REQUIRE( Factorial(0) == 0);
+    REQUIRE( Factorial(1) == 1);
+    REQUIRE( Factorial(2) == 2);
+    REQUIRE( Factorial(3) == 6);
+    REQUIRE( Factorial(10) == 3628800);
+}
