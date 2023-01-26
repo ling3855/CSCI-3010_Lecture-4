60c95d0a54c7d135c1a166c92ae5fef373ac24e1
Linda Ngo <111800906+ling3855@users.noreply.github.com>
2023-01-26 13:11:41 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 #include <vector> 
 
 TEST_CASE ("Factorials are computed", "[factorial]"){
-    REQUIRE( Factorial(0) == 0);
+    REQUIRE( Factorial(0) == 1);
     REQUIRE( Factorial(1) == 1);
     REQUIRE( Factorial(2) == 2);
     REQUIRE( Factorial(3) == 6);
