8676ca21764c27ac868d5e3a48cae1423ddf8bee
Linda Ngo <111800906+ling3855@users.noreply.github.com>
2023-01-26 13:07:49 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+#include <vector>
+#include "functions_to_implement.cpp"
+
+int main(){
+ 	
+    std::cout << NthFibonacci(2) << std::endl;
+    std::cout << NthFibonacci(5) << std::endl;
+}
